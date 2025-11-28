26d1949a05de062d9a377a12d86a090708d14d83
Andres Munoz <am@andresmunozit.com>
2025-11-28 10:48:05 -0500

    chore: fix js solution for binary search

--- kb/binary-search/js/index.js
+++ kb/binary-search/js/index.js
@@ -1,20 +1,20 @@
 const arr = [1, 3, 7, 12, 21, 33, 34, 40, 43, 52, 67, 101];
 
-// find 12
-
 function binarySearch(nums, target) {
     let left = 0;
     let right = nums.length - 1;
 
     while (left < right) {
-        let m = Math.floor((left + right)/2);
+        let m = Math.ceil((left + right)/2);
 
         if (nums[m] == target) {
             return m;
         } else if (nums[m] > target) {
-            right = m;
+            // This excludes the most right element which we already compared
+            right = m - 1;
         } else {
-            left = m;
+            // This excludes the most left element which we already compared
+            left = m + 1;
         }
     }
 
@@ -24,3 +24,6 @@ function binarySearch(nums, target) {
 console.log(binarySearch(arr, 33)) // 5
 console.log(binarySearch(arr, 43)) // 8
 console.log(binarySearch(arr, 3)) // 1
+console.log(binarySearch(arr, 22)) // -1
+console.log(binarySearch(arr, -3)) // -1
+console.log(binarySearch(arr, 200)) // -1
