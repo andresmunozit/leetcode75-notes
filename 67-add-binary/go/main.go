bc5e30237cbe5736d0764fd8a00a6aa58e190ac9
Andres Munoz <am@andresmunozit.com>
2026-01-11 20:13:27 -0500

    67: add binary (wip)

--- 67-add-binary/go/main.go
+++ 67-add-binary/go/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func addBinary(a, b string) string {
-	aNum, err := strconv.ParseInt(a, 2, 32)
-	if err != nil {
-		panic(err)
-	}
-
-	bNum, err := strconv.ParseInt(b, 2, 32)
-	if err != nil {
-		panic(err)
-	}
-
-	return fmt.Sprintf("%b", aNum+bNum)
-}
+// func addBinary(a, b string) string {
+// 	// Make arrays and reverse
+// }
 
 func main() {
-	fmt.Println(addBinary("11", "1"))      // 100
-	fmt.Println(addBinary("1010", "1011")) // 10101
+	// fmt.Println(addBinary("11", "1"))      // 100
+	// fmt.Println(addBinary("1010", "1011")) // 10101
+	fmt.Println(reverse([]string{"a", "b", "c"}))
+}
+
+func reverse(s []string) []string {
+	rev := []string{}
+	for i := len(s) - 1; i >= 0; i-- {
+		rev = append(rev, s[i])
+	}
+	return rev
 }
